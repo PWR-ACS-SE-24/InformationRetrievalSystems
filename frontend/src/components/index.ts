4faabd95069a4c322d535528bd3b64074c999551
jakubzehner <kubusja.pl@gmail.com>
2025-05-23 20:43:22 +0200

    chcę sobie strzelić w łeb

--- frontend/src/components/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as DoubleRangeSlider } from "./DoubleRangeSlider.svelte";
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,39 @@
 <script>
-  import { DoubleRangeSlider } from "$components";
+  import RangeSlider from "svelte-range-slider-pips";
 
   let slider_min = 1986; // Found in the dataset using script that probably works just fine
   let slider_max = new Date().getFullYear();
 
-  let start = slider_min;
-  let end = slider_max;
+  let values = [1986, 2025];
 </script>
 
-<DoubleRangeSlider
-  min={slider_min}
-  max={slider_max}
-  width="300px"
-  bind:start
-  bind:end
-/>
+<div class="range-wrapper">
+  <RangeSlider
+    range
+    float
+    pips
+    pushy
+    first="label"
+    last="label"
+    min={slider_min}
+    max={slider_max}
+    bind:values
+  />
+
+  <code>DEBUG: bind:values={"{values}"}</code> ( <code>{values}</code> )
+</div>
+
+<style>
+  :root {
+    --range-slider: #d7dada; /* slider main background color */
+    --range-handle-inactive: #0c8ce9; /* inactive handle color */
+    --range-handle: #0c8ce9; /* non-focussed handle color */
+    --range-handle-focus: #0c8ce9; /* focussed handle color */
+  }
+
+  .range-wrapper {
+    margin-left: 20vw;
+    margin-right: 20vw;
+    margin-top: 20vh;
+  }
+</style>
