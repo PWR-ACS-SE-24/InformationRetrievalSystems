ec4045227344301472407ecfb7fdcf3e1e72dc39
mlodybercik <przemekbarcicki@gmail.com>
2025-05-25 18:35:40 +0200

    CORS uwu

--- backend/arxivsearch/config.py
+++ backend/arxivsearch/config.py
@@ -4,3 +4,4 @@ DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+psycopg://arxivsearch:arxiv
 ELASTIC_URL = os.getenv("ELASTIC_URL", "http://localhost:9200")
 DEBUG = int(os.getenv("DEBUG", "1"))
 LOGGER_LEVEL = int(os.getenv("LOGGER_LEVEL", "10"))
+CORS = os.getenv("CORS", "http://localhost:5173").split(",")
